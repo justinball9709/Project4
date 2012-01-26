c8fb9661167301e3e1cf822a0831985ad0151fb2
justinball9709 <justinball9709@gmail.com>
2012-01-25 21:36:35 -0500

    Added variable scope function

--- Ball_Justin_Assignment4.js
+++ Ball_Justin_Assignment4.js
@@ -1,1 +1,15 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+//function
+var feedPirate = function (pirateName, food) {
+  console.log("Gave some " + food + " to the pirate " + pirateName);
+};
+
+feedPirate("Morgan", "oranges");
+
+//Example variable Scope
+var feedDog = function (dogName, food) {
+  console.log("I fed my dog " + dogName + " some " + food);
+};
+
+feedDog("Wilson", "dog food.");
