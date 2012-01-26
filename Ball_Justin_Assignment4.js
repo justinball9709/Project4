147376eaa639222808bf3e32edd89add89a2cae3
justinball9709 <justinball9709@gmail.com>
2012-01-26 00:58:27 -0500

    MAKE UP: Scope PROJECT 2

--- Ball_Justin_Assignment4.js
+++ Ball_Justin_Assignment4.js
@@ -1,15 +1,30 @@
-//alert("JavaScript works!");
+//Author: Justin Ball
+// January 26,2011
+// Project 4
 
-//function
-var feedPirate = function (pirateName, food) {
-  console.log("Gave some " + food + " to the pirate " + pirateName);
-};
-
-feedPirate("Morgan", "oranges");
-
-//Example variable Scope
+//MAKE UP: Scope / Project 2
 var feedDog = function (dogName, food) {
   console.log("I fed my dog " + dogName + " some " + food);
+var feedCat = function (catName, food){
+  console.log("Also fed " + food + " to " + catName);)
+};
+    feedCat("Chewy", "cat food");
+    feedCat("Garfield", "cat food");
+  console.log(dogName + " enjoys his " + food);
+};
+feedDog("Wilson", "dog food.");
+
+//MAKE UP: Returns /Project 2
+var buyTwix = function(givenMoney)  {
+    var smallTwix = 1,
+      bigCandy;
+    if (givenMoney < smallTwix) {
+      console.log("I can't afford Twix with that much money.");
+      return;
+}
+    bigCandy = Math.floor(givenMoney / smallTwix);
+      return bigCandy;
 };
 
-feedDog("Wilson", "dog food.");
+var affordTwix = buyTwix(10);
+console.log("Thanks for purchasing " + affordTwix + " Twix candies.");
